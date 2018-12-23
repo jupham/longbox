1646c416e52c29b275bb2df2ce9a0e1e78b97549
Jordan Upham <juph4m@gmail.com>
2018-12-23 09:20:52 -0500

    Added test grid view

---
+++ longbox/longbox/Views/Comics/ComicCell.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace longbox.Views.Comics
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ComicCell : ContentView
+	{
+		public ComicCell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
