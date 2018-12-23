9ce9389f44073498262d7016b8f6a8391945494d
Jordan Upham <juph4m@gmail.com>
2018-12-23 09:31:16 -0500

    Added comic title to grid

---
+++ longbox/longbox/ViewModels/ComicGridViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace longbox.ViewModels
+{
+    class ComicGridViewModel : BaseViewModel
+    {
+        public ComicGridViewModel ()
+        {
+            Title = "Comics";
+        }
+    }
+}
